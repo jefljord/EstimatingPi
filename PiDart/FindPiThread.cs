4e340e520d9733e06e4fd4be20b927761a0fe1d6
jeff jordan <jeffjordan33@outlook.com>
2019-10-17 12:03:03 -0500

    Add project files.

---
+++ PiDart/FindPiThread.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace PiDart
+{
+    class FindPiThread
+    {
+        private int totDart;
+        public int printTot
+        {
+            get
+            {
+                return totDart;
+
+            }
+        }
+
+        private int hitDarts;
+        public int printHits
+        {
+            get
+            {
+                return hitDarts;
+
+            }
+        }
+        
+
+        Random ran;
+
+        public FindPiThread(int xdarts)
+        {
+            hitDarts = 0;
+            ran = new Random();
+            totDart = xdarts;
+
+        }
+
+        public void throwDarts()
+        {
+            for(int i = 0; i < totDart; i++)
+            {               
+      
+                double piave = ran.NextDouble();
+                double seconddouble = ran.NextDouble();
+
+                if (Math.Sqrt((Math.Pow(piave, 2) + Math.Pow(seconddouble, 2))) <= 1)
+                {
+                    hitDarts++;
+                }
+                
+            }
+
+
+        }
+    }
+}
---
+++ PiDart/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace PiDart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number of darts to throw?");
+
+            
+            int darts = Convert.ToInt32(Console.ReadLine());
+            
+
+            Console.WriteLine("Number of threads?");
+
+            
+            int fourThread = Convert.ToInt32(Console.ReadLine());
+
+            List<Thread> multiThread = new List<Thread>();
+            List<FindPiThread> findPi = new List<FindPiThread>();
+
+            int tot = 0;
+            for (int i = 0; i < fourThread; i++)
+            {
+                FindPiThread threadDart = new FindPiThread(darts);
+
+                findPi.Add(threadDart);
+
+                Thread newthread = new Thread(new ThreadStart(threadDart.throwDarts));
+
+                multiThread.Add(newthread);
+
+                newthread.Start();
+
+                Thread.Sleep(16);
+            }
+
+            for (int i = 0; i < fourThread; i++)
+            {
+                multiThread[i].Join();
+            }
+            for (int i = 0; i < fourThread; i++)
+            {
+                Console.WriteLine(findPi[i].printHits);
+                tot += findPi[i].printHits;
+            }
+
+            Console.WriteLine(4 * ((double) tot / ((double)darts * (double)fourThread)));
+            Console.ReadLine();
+        }
+    }
+}
